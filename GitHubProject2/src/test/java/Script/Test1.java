2e1d65b78a44d94df44ac891762c5b50dd1e0d3f
Aishu <Aishu@DESKTOP-9R8S1OK>
2018-05-22 20:49:12 +0530

    Commit 1

---
+++ GitHubProject2/src/test/java/Script/Test1.java
@@ -0,0 +1,28 @@
+package Script;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1 {
+@Test
+public void InstaLogin() {
+	System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	driver.manage().window().maximize();
+	driver.get("https://www.google.com/");
+	WebElement ele = driver.findElement(By.name("q"));
+	ele.sendKeys("instagram");
+	ele.sendKeys(Keys.ENTER);
+	driver.close();
+}
+	
+
+	
+}
